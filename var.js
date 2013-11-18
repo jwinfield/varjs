ebf3794b0eacf11c1015b1f735d932e20b01ce51
Jim Winfield <jim@floop.com>
2013-11-17 22:27:13 -0500

    Initial app files

---
+++ var.js
@@ -0,0 +1,33 @@
+// Module dependencies.
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var varjs = express();
+
+// all environments
+varjs.set('port', process.env.PORT || 1221);
+varjs.set('views', path.join(__dirname, 'views'));
+varjs.set('view engine', 'jade');
+varjs.use(express.favicon());
+varjs.use(express.logger('dev'));
+varjs.use(express.json());
+varjs.use(express.urlencoded());
+varjs.use(express.methodOverride());
+varjs.use(varjs.router);
+varjs.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
+varjs.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == varjs.get('env')) {
+  varjs.use(express.errorHandler());
+}
+
+varjs.get('/', routes.index);
+varjs.get('/users', user.list);
+
+http.createServer(varjs).listen(varjs.get('port'), function(){
+  console.log('varjs listening on port ' + varjs.get('port'));
+});
